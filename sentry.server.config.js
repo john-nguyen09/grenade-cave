a58dee319beed9b439419ea58b54a9dd44fc1516
John Nguyen <john.nguyen09@outlook.com>
2023-04-21 20:22:22 +1000

    Add DSN in the log

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -6,7 +6,7 @@ import * as Sentry from '@sentry/nextjs';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
-console.log('Sentry server loaded');
+console.log(`Sentry server loaded: ${SENTRY_DSN}`);
 
 Sentry.init({
   dsn: SENTRY_DSN,
