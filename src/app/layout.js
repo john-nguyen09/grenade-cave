786ef4fcdc8b5f424c8d0622c880a6adb63629e5
John Nguyen <john.nguyen09@outlook.com>
2023-04-29 15:13:16 +1000

    Add icons to public folder

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,19 @@ import './globals.css';
 export const metadata = {
   title: 'Grenade Cave',
   description: 'Written for movie nights',
+  icons: {
+    icon: [
+      { url: '/favicon-32x32.png', sizes: '32x32' },
+      { url: '/favicon-16x16.png', sizes: '16x16' },
+    ],
+    apple: '/apple-touch-icon.png',
+    other: [
+      {
+        rel: 'manifest',
+        url: '/site.webmanifest',
+      },
+    ],
+  },
 };
 
 const inter = Inter({ subsets: ['latin'] });
